4e7850aae9770df47eac5354833a3d0d0548fdad
abhisheksachdeva <sachdeva351@gmail.com>
2016-03-10 00:28:11 +0530

    Added server code

---
+++ Server codes/myIndex.py
@@ -0,0 +1,59 @@
+from bottle import route, run, template,response, get, post,request, static_file
+import os
+# from config import *
+import time
+import requests
+import json
+import csv
+from urlparse import urlparse,parse_qs
+
+url = '172.25.13.239'
+
+@route('/')
+def home():
+	msg = 'Home'
+	return msg
+
+
+@post('/id')
+def sendToCLient():
+	no=request.forms.get('q')
+	print no
+	# query_components = parse_qs(urlparse(url+'/id?q=2').query)
+	# # id = int ()
+	# print query_components['q']
+	readFile= open('serverData.csv','rb')
+	reader = csv.reader(readFile)
+	lines=[l for l in reader]
+	name ='Hello'
+	for line in lines:
+                if int(line[0])==int (no):
+                        print "Name is ",line[1]
+                        print "Id is ",line[0]
+                        print "Your balace is" , line[2]
+                        name=  "Name is "+line[1] +"\nId is "+line[0] + "\nYour balance is "+line[2]
+                                                  
+	readFile.close()
+	return name
+
+@post('/user')
+def receiveFile():
+	userData = request.files.get('report')
+	print userData.filename
+	file_path = "serverData.csv"
+	if os.path.exists("serverData.csv"):
+		os.remove("serverData.csv")
+		userData.save(file_path)
+    # else:
+    # 	userData.save(file_path)
+
+
+@post('/getfile')
+def getFile():
+	file= open('serverData.csv',"rb")
+	return file
+
+run(host=url, port=8080, debug=True)
+
+
+

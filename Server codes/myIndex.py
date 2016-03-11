3c89ba27ecffbd17bfa4565c00b86fd773dd7bb9
abhisheksachdeva <sachdeva351@gmail.com>
2016-03-12 00:36:11 +0530

    Added SMS alerts

--- Server codes/myIndex.py
+++ Server codes/myIndex.py
@@ -5,9 +5,12 @@ import requests
 import json
 import csv
 from urlparse import urlparse,parse_qs
+from twilio.rest import TwilioRestClient
 
-url = '172.25.13.239'
-
+##url = '10.42.0.1'
+url='172.25.13.239'
+account_sid = "AC8d882210cd24e4b2a437d31f35018fa1"
+auth_token = "5b65f7d6c7a60c7bc2d3b5ace89e0f29"
 @route('/')
 def home():
 	msg = 'Home'
@@ -35,11 +38,25 @@ def sendToCLient():
 @post('/user')
 def receiveFile():
 	userData = request.files.get('report')
+	userID= request.forms.get('q')
+	print userID
 	print userData.filename
 	file_path = "serverData.csv"
 	if os.path.exists("serverData.csv"):
 		os.remove("serverData.csv")
 		userData.save(file_path)
+	readFile= open('serverData.csv','rb')
+	reader = csv.reader(readFile)
+	lines=[l for l in reader]
+	for line in lines:
+                if int(line[0])==int (userID):
+                        client = TwilioRestClient(account_sid, auth_token)
+			message = client.messages.create(to="++917060334386", from_="+12248032729",
+                                     body="\nA/c holder: "+line[1]+"\nTransaction Successful for ID "+line[0]+". Balance is "+line[2])
+                                                  
+	readFile.close()
+	
+
 
 @post('/getfile')
 def getFile():
